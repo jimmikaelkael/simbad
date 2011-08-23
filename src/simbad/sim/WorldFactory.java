b19f17137a91dd85f94895fa557408ebe3f9b7a6
jimmikaelkael <jimmikaelkael@wanadoo.fr>
2011-08-23 20:11:16 +0200

    added setRoboSoccerEnvironment() to WorldFactory class

--- src/simbad/sim/WorldFactory.java
+++ src/simbad/sim/WorldFactory.java
@@ -24,8 +24,75 @@
  */
 package simbad.sim;
 
-
+import javax.vecmath.Color3f;
+import javax.vecmath.Vector3d;
 /**
  *
  */
-public class WorldFactory extends Factory {}
+public class WorldFactory extends Factory {
+
+	/** Sets a RoboSoccer terrain in the given environment.
+	 * 
+	 * @param environment to set.
+	 */ 
+	static public void setRoboSoccerEnvironment(EnvironmentDescription wd) {
+
+		// set lights, floor and wall color, and set physics
+		wd.light1IsOn = true;
+		wd.light2IsOn = false;
+		wd.floorColor = wd.green;
+		wd.wallColor = wd.white;
+		wd.showAxis(false);
+		wd.setUsePhysics(true);
+
+		// add walls
+		Wall w1 = new Wall(new Vector3d(-8, 0, 0), 18.3f, 1, wd);
+		w1.rotate90(1);
+		Wall w2 = new Wall(new Vector3d(8, 0, 0), 18.3f, 1, wd);
+		w2.rotate90(1);
+		wd.add(w1);
+		wd.add(w2);
+		wd.add(new Wall(new Vector3d(-5, 0, 9), 6, 1, wd));
+		wd.add(new Wall(new Vector3d(5, 0, 9), 6, 1, wd));
+		wd.add(new Wall(new Vector3d(-5, 0, -9), 6, 1, wd));
+		wd.add(new Wall(new Vector3d(5, 0, -9), 6, 1, wd));
+
+		// add goals
+		wd.add(new Wall(new Vector3d(0, 0, -10), 4, 2, wd));
+		wd.add(new Wall(new Vector3d(0, 0, 10), 4, 2, wd));
+		Wall w3 = new Wall(new Vector3d(-2, 0, -9.5f), 1.33f, 2, wd);
+		w3.rotate90(1);
+		Wall w4 = new Wall(new Vector3d(2, 0, -9.5f), 1.33f, 2, wd);
+		w4.rotate90(1);
+		Wall w5 = new Wall(new Vector3d(-2, 0, 9.5f), 1.33f, 2, wd);
+		w5.rotate90(1);
+		Wall w6 = new Wall(new Vector3d(2, 0, 9.5f), 1.33f, 2, wd);
+		w6.rotate90(1);
+		wd.add(w3);
+		wd.add(w4);
+		wd.add(w5);
+		wd.add(w6);
+
+		// add lines
+		Line l1 = new Line(new Vector3d(-8, 0, 0), 16, wd, new Color3f(2.5f,2.5f,2.5f));
+		l1.rotate90(1);
+		Line l2 = new Line(new Vector3d(-2, 0, -9), 4, wd, new Color3f(2.5f,2.5f,2.5f));
+		l2.rotate90(1);
+		Line l3 = new Line(new Vector3d(-2, 0, 9), 4, wd, new Color3f(2.5f,2.5f,2.5f));
+		l3.rotate90(1);
+		Line l4 = new Line(new Vector3d(-3.5f, 0, -6.5f), 7, wd, new Color3f(2.5f,2.5f,2.5f));
+		l4.rotate90(1);
+		Line l5 = new Line(new Vector3d(-3.5f, 0, 6.5f), 7, wd, new Color3f(2.5f,2.5f,2.5f));
+		l5.rotate90(1);
+		wd.add(l1);
+		wd.add(l2);
+		wd.add(l3);
+		wd.add(l4);
+		wd.add(l5);
+		wd.add(new Line(new Vector3d(-3.5f, 0, -9), 2.5f, wd, new Color3f(2.5f,2.5f,2.5f)));
+		wd.add(new Line(new Vector3d(3.5f, 0, -9), 2.5f, wd, new Color3f(2.5f,2.5f,2.5f)));
+		wd.add(new Line(new Vector3d(-3.5f, 0, 6.5f), 2.5f, wd, new Color3f(2.5f,2.5f,2.5f)));
+		wd.add(new Line(new Vector3d(3.5f, 0, 6.5f), 2.5f, wd, new Color3f(2.5f,2.5f,2.5f)));
+	}
+
+}
